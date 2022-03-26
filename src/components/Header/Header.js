6528dc3761a695ee000be38bf2c042ff10bd8b5b
rahad2694 <rahad.haw@gmail.com>
2022-03-26 06:46:25 +0600

    Header added & Bootstrap installed

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Header = () => {
+    return (
+        <nav className='bg-dark py-3'>
+            <h1 className='text-white'>Interplanetary spaceflight</h1>
+        </nav>
+    );
+};
+
+export default Header;
